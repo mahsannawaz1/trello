32e0da882742de20919e8832d6f8aa45d55277c8
Muhammad Ahsan <nawazehsen@gmail.com>
2024-10-13 10:37:23 -0700

    Pushing code to github

---
+++ app/src/main/java/com/example/trello_clone/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.example.trello_clone
+
+import android.os.Bundle
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_profile)
+
+    }
+}
+
