da248370d2a9e3c7f57e29a964c697def51689a6
Muhammad Ahsan <nawazehsen@gmail.com>
2024-10-20 11:13:23 -0700

    fixed some bugs

--- app/src/main/java/com/example/trello_clone/EditBoard.kt
+++ app/src/main/java/com/example/trello_clone/EditBoard.kt
@@ -4,7 +4,9 @@ package com.example.trello_clone
 import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
+import android.widget.GridLayout
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class EditBoard : AppCompatActivity() {
@@ -24,6 +26,20 @@ class EditBoard : AppCompatActivity() {
                 else -> false
             }
         }
+
+        var goToLabels = findViewById<LinearLayout>(R.id.lableGrid)
+
+        goToLabels.setOnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val intent = Intent(this, Labels::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
 
